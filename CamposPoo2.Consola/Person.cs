658d9fa618d65f502680ef74890c5d746a694b52
Lompy <lombardolucca1@gmail.com>
2025-07-16 18:15:59 -0300

    Agregamos Campos y Static

---
+++ CamposPoo2.Consola/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CamposPoo2.Consola
+{
+    internal class Person
+    {
+        private static int NroId =0;
+        public string Name { get; set; }
+        public int Id { get; set; }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new ArgumentException("La edad debe ser mayor a 0.");
+
+                }
+            }
+
+        }
+
+        public Person(string name, int age)
+        {
+            NroId++;
+            this.Name = name;
+            this.Age = age;
+            this.Id = NroId;
+            
+        }
+        public void MostrarInfo()
+        {
+            Console.WriteLine($"Nombre: {Name}");
+            Console.WriteLine($"Edad: {Age} años");
+            Console.WriteLine($"Id: {Id}");
+
+        }
+    }
+}
+
---
+++ CamposPoo2.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace CamposPoo2.Consola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person juan = new Person("Juan", 8);
+                Person carlos = new Person("Carlos", 35);
+                juan.MostrarInfo();
+                Console.WriteLine("");
+                carlos.MostrarInfo();
+
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+
+            }
+        }
+    }
+}
